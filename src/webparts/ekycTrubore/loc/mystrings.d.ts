c51598a891a9f4f25fb2403768bcaf605d894741
AniketT2201 <Aniket@isritechnologies.com>
2025-11-20 10:59:17 +0530

    .

---
+++ src/webparts/ekycTrubore/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEkycTruboreWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EkycTruboreWebPartStrings' {
+  const strings: IEkycTruboreWebPartStrings;
+  export = strings;
+}
